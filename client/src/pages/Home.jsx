859e2cf4582f7a38d2309773b0a49a1398c1d628
Arbab khan <ak2077719@gmail.com>
2023-03-10 12:16:17 +0530

    added server url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   const fetchPosts = async () =>{
     setloading(true);
     try {
-      const response = await fetch('http://localhost:8000/api/v1/post',{
+      const response = await fetch('https://dall-e-8y9s.onrender.com/api/v1/post',{
         method:'GET',
         headers:{
           'Content-Type': 'application/json',
